be42209d4bab9b66791ed54454de529d72046db8
wpoth <302758116@student.rocmondriaan.nl>
2023-11-10 15:50:28 +0100

    begin opdrachten javascript

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// opdracht 1 
+let salary = 2000;
+let Higher = 0.5;
+let result = document.querySelector(".whoop")
+
+let awnser1 = salary * Higher;
+
+result.textContent = awnser1;
+// opdracht 2
+
+let house = 400000;
+let rent = 0.25
+
+let awnser2 = house * rent;
+
+let result2 = document.querySelector(".opdr2");
+result2.textContent = awnser2;
+
+// opdracht 3 
+let jacket = 100;
+let rent2 = 0.21;
+
+awnser3 = jacket * rent2;
+
+let result3 = document.querySelector(".opdr3")
+
+result3.textContent = awnser3;
+
+
+//opdracht 4 
+
+let prompt = prompt()
+if(prompt === 'docent') 
+(console.log("u bent een docent"))
